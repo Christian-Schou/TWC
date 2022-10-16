e170710c309cc0fadb3e18a05e38f2de55e5400b
Christian Schou <christian.schou@hotmail.com>
2022-10-16 12:51:50 +0200

    Added Vehicle entity based on faker.js

---
+++ Tutorials/ASP_NET_Core_6/In-Memory-Caching/In-Memory-Caching/Entities/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace In_Memory_Caching.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? Type { get; set; }
+        public string? Color { get; set; }
+        public string? VIN { get; set; }
+        public string? VRM { get; set; }
+    }
+}
+
