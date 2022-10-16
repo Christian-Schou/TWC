f17a213eb972e9021da751a895abdd2267c0cfe7
Christian Schou <christian.schou@hotmail.com>
2022-10-16 14:21:18 +0200

    Fixed API implementation of fields

--- Tutorials/ASP_NET_Core_6/In-Memory-Caching/Entities/Vehicle.cs
+++ Tutorials/ASP_NET_Core_6/In-Memory-Caching/Entities/Vehicle.cs
@@ -4,7 +4,6 @@ namespace In_Memory_Caching.Entities
     public class Vehicle
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
         public string? Manufacturer { get; set; }
         public string? Model { get; set; }
         public string? Type { get; set; }
