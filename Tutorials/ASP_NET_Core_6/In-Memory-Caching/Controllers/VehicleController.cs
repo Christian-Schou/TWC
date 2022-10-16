15ac7b57675f7e0a36f7c4fc6bbd94045b103dbb
Christian Schou <christian.schou@hotmail.com>
2022-10-16 14:26:33 +0200

    Added vehicle service and controller
    Added default implementation for vehicle service and controller with GetAll() endpoint

---
+++ Tutorials/ASP_NET_Core_6/In-Memory-Caching/Controllers/VehicleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using In_Memory_Caching.Entities;
+using In_Memory_Caching.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace In_Memory_Caching.Controllers
+{
+    [Route("api/[controller]")]
+    public class VehicleController : Controller
+    {
+        private readonly IVehicleService _vehicleService;
+
+        public VehicleController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            List<Vehicle> vehicles = await _vehicleService.GetAllAsync();
+
+            return Ok(vehicles);
+        }
+    }
+}
+
---
+++ Tutorials/ASP_NET_Core_6/In-Memory-Caching/Services/VehicleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using In_Memory_Caching.Data;
+using In_Memory_Caching.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace In_Memory_Caching.Services
+{
+    public interface IVehicleService
+    {
+        Task<List<Vehicle>> GetAllAsync();
+    }
+
+    public class VehicleService : IVehicleService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VehicleService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Vehicle>> GetAllAsync()
+        {
+            var vehicles = await _dbContext.Vehicles
+                .ToListAsync();
+
+            return vehicles;
+        }
+    }
+}
+
